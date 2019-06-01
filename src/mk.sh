600cc880ff5c63db86c264c7e95ebed27721079c
Vedran Novakovic <venovako@venovako.eu>
2019-06-01 20:54:42 +0200

    Enable the multi-arch argument.

--- src/mk.sh
+++ src/mk.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Some possible flags: STD=14 ANIMATE=1 HOST_FLAGS="-ffp-contract=on,-integrated-as"
-make -C $1 GPU_ARCH=$2 NDEBUG=$3 CVG=$4 $5
+make -C $1 GPU_ARCH="$2" NDEBUG=$3 CVG=$4 $5
