cbab099c18c085c6f75405d713dd2943511b3342
Vedran Novaković <venovako@venovako.eu>
2020-10-13 17:31:11 +0200

    several fixes and enhancements

--- var/mk_test.sh
+++ var/mk_test.sh
@@ -5,7 +5,7 @@ scl enable devtoolset-9 "nvcc -DNDEBUG -DUSE_CUSOLVER -Ishared -std c++17 -restr
 echo "done."
 
 echo "Building test_zhegvd.exe ..."
-scl enable devtoolset-9 "nvcc -DNDEBUG -DUSE_CUSOLVER -Ishared -std c++17 -restrict -O3 --extra-device-vectorization -res-usage -Xcompiler -march=native -Xcompiler -pthread -gencode arch=compute_$1,code=sm_$1 test_dsygvd.cu shared/cuda_helper.cu shared/my_utils.cu -o test_zhegvd.exe -l cusolver,cublas,dl,m"
+scl enable devtoolset-9 "nvcc -DNDEBUG -DUSE_CUSOLVER -Ishared -std c++17 -restrict -O3 --extra-device-vectorization -res-usage -Xcompiler -march=native -Xcompiler -pthread -gencode arch=compute_$1,code=sm_$1 test_zhegvd.cu shared/cuda_helper.cu shared/my_utils.cu -o test_zhegvd.exe -l cusolver,cublas,dl,m"
 echo "done."
 
 echo "Building test_compare.exe ..."
