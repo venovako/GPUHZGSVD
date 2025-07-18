e267b4ea004cab08a90c7f120a3d518ee61bca31
Vedran Novakovic <venovako@venovako.eu>
2025-07-18 15:47:40 +0200

    further updates

--- src/mk.sh
+++ src/mk.sh
@@ -1,15 +1,3 @@
 #!/bin/bash
 # Some possible flags: ANIMATE=1 PROFILE="$2"
-GCCMAJORVER=`gcc -dumpversion | cut -d. -f1`
-if [ "$GCCMAJORVER" != "4" ]
-then
-	if [ "$GCCMAJORVER" != "9" ]
-	then
-		make -C $1 GPU_ARCH="$2" NDEBUG=$3 CVG=$4 STD=14 $5
-	else
-		make -C $1 GPU_ARCH="$2" NDEBUG=$3 CVG=$4 STD=17 $5
-	fi
-else
-	make -C $1 GPU_ARCH="$2" NDEBUG=$3 CVG=$4 $5
-fi
-unset GCCMAJORVER
+make -C $1 GPU_ARCH="$2" NDEBUG=$3 CVG=$4 $5
--- var/mk.sh
+++ var/mk.sh
@@ -1,14 +1,2 @@
 #!/bin/bash
-GCCMAJORVER=`gcc -dumpversion | cut -d. -f1`
-if [ "$GCCMAJORVER" != "4" ]
-then
-	if [ "$GCCMAJORVER" != "9" ]
-	then
-		make -C $1 GPU_ARCH="$2" NDEBUG=$3 STD=14 $4
-	else
-		make -C $1 GPU_ARCH="$2" NDEBUG=$3 STD=17 $4
-	fi
-else
-	make -C $1 GPU_ARCH="$2" NDEBUG=$3 $4
-fi
-unset GCCMAJORVER
+make -C $1 GPU_ARCH="$2" NDEBUG=$3 $4
